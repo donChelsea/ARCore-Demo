9eb63c67a7f7845755141175cc0e7ad0d5aa1311
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2019-12-13 01:26:08 -0500

    add pointer

--- app/src/main/java/com/example/arcoredemo/MainActivity.java
+++ app/src/main/java/com/example/arcoredemo/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.arcoredemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.ar.core.Frame;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.ux.ArFragment;
 
 public class MainActivity extends AppCompatActivity {
+    ArFragment arFragment;
+    private PointerDrawable pointer = new PointerDrawable();
+    private boolean isTracking; // indicates if arcore is in tracking state
+    private boolean isHitting; // indicates that the user is looking at a plane
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+        arFragment.getArSceneView().getScene().addOnUpdateListener(new Scene.OnUpdateListener() {
+            @Override
+            public void onUpdate(FrameTime frameTime) {
+                arFragment.onUpdate(frameTime);
+                onUpdate(); 
+            }
+        });
     }
+
+    private void onUpdate() {
+        // if ar core is not tracking, remove the pointer until tracking is restored
+        // if yes, checking if ar core is hitting a plane to place pointer
+
+        boolean trackingChanged = updateTracking();
+        View contentView = findViewById(android.R.id.content);
+        if (trackingChanged) {
+            if (isTracking) {
+                contentView.getOverlay().add(pointer);
+            } else {
+                contentView.getOverlay().remove(pointer);
+            }
+            contentView.invalidate();
+        }
+
+        if (isTracking) {
+            boolean hitTestChanged = updateHitTest();
+            if (hitTestChanged) {
+                pointer.setEnabled(isHitting);
+                contentView.invalidate();
+            }
+        }
+    }
+
+    private boolean updateTracking() {
+        // uses ar core's camera state and returns true if the tracking state has changed since last call
+
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        boolean wasTracking = isTracking;
+        isTracking = frame != null && frame.getCamera().getTrackingState() == TrackingState.TRACKING;
+        return isTracking != wasTracking;
+    }
+
 }
