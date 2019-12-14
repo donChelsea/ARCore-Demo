862a594c96ffe7c86f3ac4aeb95920aae76287fc
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2019-12-14 17:51:34 -0500

    added models

--- app/src/main/java/com/example/arcoredemo/MainActivity.java
+++ app/src/main/java/com/example/arcoredemo/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.arcoredemo;
 
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.Trackable;
 import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private PointerDrawable pointer = new PointerDrawable();
     private boolean isTracking; // indicates if arcore is in tracking state
     private boolean isHitting; // indicates that the user is looking at a plane
-
+    ModelLoader modelLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.onUpdate();
             }
         });
+
+        modelLoader = new ModelLoader(new WeakReference<>(this));
+
+        initalizeGallery();
     }
 
-    // pointer creation
+    // POINTER CREATION
 
     // if ar core is not tracking, remove the pointer until tracking is restored
     // if yes, checking if ar core is hitting a plane to place pointer
@@ -98,6 +112,59 @@ public class MainActivity extends AppCompatActivity {
         return new android.graphics.Point(vw.getWidth() / 2, vw.getHeight() / 2);
     }
 
+    // SCENE CREATION
 
+    // creates the gallery layout and adds the model to the view
+    public void initalizeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
 
+        ImageView andy = new ImageView(this);
+        andy.setImageResource(R.drawable.droid_thumb);
+        andy.setContentDescription("andy");
+        andy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addObject(Uri.parse("andy_dance.sfb"));
+            }
+        });
+        gallery.addView(andy);
+    }
+
+    // performs the task of adding the model
+    private void addObject(Uri model) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        Point point = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(point.x, point.y);
+            for (HitResult hit: hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    modelLoader.loadModel(hit.createAnchor(), model);
+                }
+                break;
+            }
+        }
+    }
+
+    // anchor nodes are added to hold the position of the model on the scene
+    // transformation nodes adds functionality to model
+    public void addNodeToScene(Anchor anchor, ModelRenderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    // will fail if network is down
+    public void onException(Throwable throwable){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(throwable.getMessage())
+                .setTitle("Error!");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return;
+    }
 }
---
+++ app/src/main/java/com/example/arcoredemo/ModelLoader.java
@@ -0,0 +1,45 @@
+package com.example.arcoredemo;
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+
+import java.lang.ref.WeakReference;
+
+// takes care of the asynchronous loading of the 3d models.
+// made as a weak reference to respect activity lifecycle
+
+public class ModelLoader {
+    private final WeakReference<MainActivity> owner;
+    private static final String TAG = "ModelLoader";
+
+    ModelLoader(WeakReference<MainActivity> owner) {
+        this.owner = owner;
+    }
+
+    void loadModel(Anchor anchor, Uri uri) {
+        if (owner.get() == null) {
+            Log.d(TAG, "Activity is null.  Cannot load model.");
+            return;
+        }
+        ModelRenderable.builder()
+                .setSource(owner.get(), uri)
+                .build()
+                .handle((renderable, throwable) -> {
+                    MainActivity activity = owner.get();
+                    if (activity == null) {
+                        return null;
+                    } else if (throwable != null) {
+                        activity.onException(throwable);
+                    } else {
+                        activity.addNodeToScene(anchor, renderable);
+                    }
+                    return null;
+                });
+
+        return;
+    }
+
+}
